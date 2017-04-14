c465fb400bf283ce485663d6c88c0cfe692d0f98
EchoGaia <maxime.rodrigues.pro@gmail.com>
2017-04-14 23:00:01 +0200

    Update abilities.js
    All abilities coded :) I think they should work normally.

--- mods/jillian/abilities.js
+++ mods/jillian/abilities.js
@@ -76,7 +76,7 @@ exports.BattleAbilities = {
 		name: "Gear Force",
 		rating: 4.5,
     },
-		          "coldblood": {
+		   "coldblood": {
 		              desc: "The power of Bug, Dark and Ghost-type attacks against this Pokemon is halved",
 		              shortDesc: "The power of Bug, Dark and Ghost-type attacks against this Pokemon is halved",
 		              onBasePowerPriority: 7,
@@ -92,7 +92,7 @@ exports.BattleAbilities = {
 	            "crushcoral": {
 		              shortDesc: "This Pokemon's Special Attack is doubled.",
 		              onModifyAtkPriority: 5,
-		              onModifyAtk: function (spatk) {
+		              onModifyAtk: function (SpA) {
 			            return this.chainModify(2);
 		},
 		id: "crushcoral",
@@ -101,15 +101,9 @@ exports.BattleAbilities = {
 		},
                 "medicine": {
                      shortDesc: "On switch-in, this pokemon cures itself and its teammates of status",
-                     onStart: function (pokemon) {
-                    let foeactive = pokemon.side.foe.active;
-                    let activated = false;
-                    for (let i = 0; i < foeactive.length; i++) {
-                       if (!foeactive[i] || !this.isAdjacent(foeactive[i], pokemon)) continue;
-                       if (!activated) {
-                           this.add('-ability', pokemon, 'Medicine', 'boost');
-                           activated = true;
-                           this.useMove(215, pokemon);
+                     onStart: function (pokemon)
+			      this.add('-ability', pokemon, 'Medicine'),
+                              this.useMove("Heal Bell", pokemon);
                 }
             }
         },
@@ -128,4 +122,140 @@ exports.BattleAbilities = {
       id: "predator",
       name: "Predator",
       rating: 3,
-};  
+      },
+	"gatesofhell": {
+		       shortDesc: "On switch-in, this pokemon uses Curse and takes halved damage from Fairy type moves",
+	               },
+		          onStart: function (pokemon) {
+			           this.useMove("Curse", pokemon);
+	     }
+		  onBasePowerPriority: 7,
+		  onSourceBasePower: function (basePower, attacker, defender, move) {
+			                   if (move.type === 'Fairy') {
+				                 return this.chainModify(0.5);
+					   }
+      },
+      id: "gatesofhell",
+      name: "Gates of Hell",
+      rating: 3.5
+      },
+	 "gatesofheaven": {
+		       shortDesc: "On switch-in, this pokemon heals back 1/3 of its max HP and takes halved damage from Dark type moves",
+	               },
+			 onStart: function (pokemon) {
+			pokemon.heal(pokemon.maxhp / 3);
+	      }
+                  onBasePowerPriority: 7,
+		  onSourceBasePower: function (basePower, attacker, defender, move) {
+			                   if (move.type === 'Dark') {
+				                 return this.chainModify(0.5);
+					   }
+     },
+     id: "gatesofheaven",
+     name: "Gates of Heaven",
+     rating: 3.5
+     },
+	 "leadership": {
+		     shortDesc: "This Pokemon's attacks get a x1.3 power boost if it moves first in the turn"
+	             },
+			     onBasePowerPriority: 8,
+		             onBasePower: function (basePower, attacker, defender, move) {
+			            if (!this.willMove(attacker)) {
+				       this.debug('Leadership boost');
+				       return this.chainModify([0x14CD, 0x1000]);
+			}
+      },
+      id: "leadership",
+      name: "Leadership",
+      rating: 2
+      },
+         "fiensdishshield": {
+		            shortDesc: "This pokemon bounces back status moves to the user. Damage taken from sup.eff moves are reduced by 30%",
+	                    },
+                          onTryHitPriority: 1,
+		          onTryHit: function (target, source, move) {
+			       if (target === source || move.hasBounced || !move.flags['reflectable']) {
+				      return;
+			}
+			let newMove = this.getMoveCopy(move.id);
+			newMove.hasBounced = true;
+			newMove.pranksterBoosted = false;
+			this.useMove(newMove, target, source);
+			return null;
+		},
+		onAllyTryHitSide: function (target, source, move) {
+			if (target.side === source.side || move.hasBounced || !move.flags['reflectable']) {
+				return;
+			}
+			let newMove = this.getMoveCopy(move.id);
+			newMove.hasBounced = true;
+			newMove.pranksterBoosted = false;
+			this.useMove(newMove, this.effectData.target, source);
+			return null;
+		},
+		effect: {
+			duration: 1,
+		     }
+		},			
+	onSourceModifyDamage: function (damage, source, target, move) {
+			if (move.typeMod > 0) {
+				this.debug('Fiendish Shield neutralize');
+				return this.chainModify(0.70);
+			}
+		},
+                id: "fiendishshield",
+	        name: "Fiendish Shield",
+	        rating: 3.5
+	 },
+	"heavenlyshield": {
+	             shortDesc: "Heals the user by 1/6 of its max HP per turn, reduces damage from sup.eff attacks by 30%"
+	              },
+				onResidualOrder: 5,
+		                onResidualSubOrder: 2,
+		                onResidual: function (pokemon) {
+			                 this.heal(pokemon.maxhp / 6);
+		     }
+		},			
+	onSourceModifyDamage: function (damage, source, target, move) {
+			if (move.typeMod > 0) {
+				this.debug('Heavenly Shield neutralize');
+				return this.chainModify(0.70);
+			}
+		},
+	        id: "heavenlyshield",
+	        name: "Heavenly Shield",
+	        rating: 3.5
+        },
+	"finalflicker": {
+		    shortDesc: "If the pokemon is Koed by a move, it uses Burn Up upon fainting",
+	             },
+		               onAfterDamageOrder: 1,
+		               onAfterDamage: function (damage, target, source, move) {
+			              if (source && source !== target && move && move.effectType === 'Move' && !target.hp) {
+		                      this.useMove("Burn Up", pokemon);
+			  }
+		 },
+		 id: "finalflicker"
+		 name: "Final Flicker"
+		 rating: 4
+	},
+        "magnetism": {
+		 shortDesc: "On switch-in, this pokemon uses Magnet Rise, and its foes becomes weak to Electric for 5 turns",
+		             },
+		          onStart: function (pokemon) {
+			           this.useMove("Magnet Rise", pokemon);
+			  }
+                    },
+			onModifyDamage: function (damage, source, target, move) {
+		             if (move.type === 'Electric') {
+		                this.debug('Magnetism boost');
+				return this.chainModify(2);
+	            },
+	            effect: {
+		       duration: 5,
+	        }
+	},
+	id: "magnetism",
+	name: "Magnetism",
+	rating: 5		
+};                
