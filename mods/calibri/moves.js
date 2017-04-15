f561acc84b0ef4b3f1063ebbc2c80bef40942c80
charizard8888 <omkumr082001@outlook.com>
2017-04-15 22:15:54 +0530

    Readdd moves.js [fixed]

--- mods/calibri/moves.js
+++ mods/calibri/moves.js
@@ -1,1 +1,95 @@
+/*
+List of flags and their descriptions:
+authentic: Ignores a target's substitute.
+bite: Power is multiplied by 1.5 when used by a Pokemon with the Ability Strong Jaw.
+bullet: Has no effect on Pokemon with the Ability Bulletproof.
+charge: The user is unable to make a move between turns.
+contact: Makes contact.
+defrost: Thaws the user if executed successfully while the user is frozen.
+distance: Can target a Pokemon positioned anywhere in a Triple Battle.
+gravity: Prevented from being executed or selected during Gravity's effect.
+heal: Prevented from being executed or selected during Heal Block's effect.
+mirror: Can be copied by Mirror Move.
+nonsky: Prevented from being executed or selected in a Sky Battle.
+powder: Has no effect on Grass-type Pokemon, Pokemon with the Ability Overcoat, and Pokemon holding Safety Goggles.
+protect: Blocked by Detect, Protect, Spiky Shield, and if not a Status move, King's Shield.
+pulse: Power is multiplied by 1.5 when used by a Pokemon with the Ability Mega Launcher.
+punch: Power is multiplied by 1.2 when used by a Pokemon with the Ability Iron Fist.
+recharge: If this move is successful, the user must recharge on the following turn and cannot make a move.
+reflectable: Bounced back to the original user by Magic Coat or the Ability Magic Bounce.
+snatch: Can be stolen from the original user and instead used by another Pokemon using Snatch.
+sound: Has no effect on Pokemon with the Ability Soundproof.
+*/
 
+'use strict';
+
+exports.BattleMovedex = {
+    "mythicalaura": {
+          num: 1000,
+          accuracy: true,
+          basePower: 0,
+          category: "Status",
+          desc: "Raises the user's accuracy by 1 stage and allows Dragon type moves to hit Fairy type Pokemon. This effect is canceled if Odor Sleuth, Foresight or Miracle Eye is used.",
+          shortDesc: "Raises the user's accuracy by 1; Dragon can hit Fairy types.",
+          id: "miracleeye",
+          name: "Miracle Eye",
+          pp: 20,
+          priority: 0,
+          flags: {protect: 1, reflectable: 1, mirror: 1, authentic: 1},
+          volatileStatus: 'miracleeye',
+          onTryHit: function (target) {
+            if (target.volatiles['foresight']) return false;
+          },
+          effect: {
+            noCopy: true,
+            onStart: function (pokemon) {
+              this.add('-start', pokemon, 'Miracle Eye');
+            },
+            onNegateImmunity: function (pokemon, type) {
+              if (pokemon.hasType('Fairy') && type === 'Dragon') return false;
+            },
+            onModifyBoost: function (boosts) {
+              if (boosts.evasion && boosts.evasion > 0) {
+                boosts.evasion = 0;
+              }
+            },
+          },
+          secondary: {
+                      chance: 100,
+                      self: {
+                       boosts: {
+                        acc: 1,
+                },
+            },
+          },
+          target: "normal",
+          type: "Dragon",
+    },
+    "energygamble": {
+		num: 1001,
+		accuracy: 100,
+		basePower: 0,
+		category: "Special",
+		desc: "Doesn't check accuracy. Becomes either a 30 BP or 150 BP move when used. Locked Base Power of 200 when used as a Z-Move.",
+		shortDesc: "50% chance of becoming 30 BP, 50% chance of becoming 150 BP.",
+		id: "energygamble",
+		name: "energygamble",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, distance: 1},
+		onModifyMove: function (move, pokemon, target) {
+			let rand = this.random(10);
+			if (rand < 6) {
+				move.basePower = 30;
+			} else if (rand > 6) {
+				move.basePower = 150;
+			}
+		},
+		secondary: false,
+		target: "normal",
+		type: "Psychic",
+		zMovePower: 200,
+		contestType: "Cool",
+	},
+    
+};
