d974b46e953d28f92805fa0571036b169b4b8ea4
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-04-16 19:38:19 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -2972,7 +2972,7 @@ exports.Formats = [
 	},
 	{
 		name: "[Gen 7] Last Will",
-		desc: ["&bullet; Every Pokemon will use the move in their last moveslot before fainting in battle."],
+		desc: ['&bullet; <a href="http://www.smogon.com/forums/threads/3601362/">Last Will</a>: Every Pokemon will use the move in their last moveslot before fainting in battle.'],
 		ruleset: ['[Gen 7] OU'],
 		mod: 'lastwill',
 	},
