969b190cc300a1f98c9ee48cf9745f86608addf8
EchoGaia <maxime.rodrigues.pro@gmail.com>
2017-04-16 20:21:55 +0200

    Create statuses.js
    Poison Scent new weather.

---
+++ mods/jillian/statuses.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports.BattleStatuses = {
+
+         "poisonscent": {
+		                     effectType: 'Weather',
+		                     duration: 5,
+                       },
+		                   onStart: function (battle, source, effect) {
+			                     if (effect && effect.effectType === 'Ability') {
+				                    if (this.gen <= 5) this.effectData.duration = 0;
+				                     this.add('-weather', 'Poison Scent', '[from] ability: ' + effect, '[of] ' + source);
+			                       } else {
+				                     this.add('-weather', 'Poison Scent');
+			                    }
+		                   },
+                       onResidualOrder: 1,
+		                    onResidual: function () {
+			                  this.add('-weather', 'Poison Scent', '[upkeep]');
+			                  if (this.isWeather('Poison Scent')) this.eachEvent('Weather');
+		                    },
+		                    onWeather: function (target) {
+                        let typeMod = this.clampIntRange(pokemon.runEffectiveness('Poison'), -6, 6);
+				                this.damage(pokemon.maxhp * Math.pow(2, typeMod) / 8);
+		                    },
+		                    onEnd: function () {
+			                   this.add('-weather', 'none');
+		                   },
+ };
