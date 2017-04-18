efe4201a6bc7f792a66fd4ebbdc7b4afa265a695
EchoGaia <maxime.rodrigues.pro@gmail.com>
2017-04-18 16:37:45 +0200

    Update statuses.js
    Code for the weather Poisoned Scent updated. (Hope it's just a syntax problem)

--- mods/jillian/statuses.js
+++ mods/jillian/statuses.js
@@ -2,33 +2,33 @@
 
 exports.BattleStatuses = {
 
-         "poisonscent": {
-		                     effectType: 'Weather',
-		                     duration: 5,
-                       },
-		                   onStart: function (battle, source, effect) {
-			                     if (effect && effect.effectType === 'Ability') {
-				                    if (this.gen <= 5) this.effectData.duration = 0;
-				                     this.add('-weather', 'Poison Scent', '[from] ability: ' + effect, '[of] ' + source);
-			                       } else {
-				                     this.add('-weather', 'Poison Scent');
-			                    }
-		                   },
-                       onResidualOrder: 1,
-		                    onResidual: function () {
-			                  this.add('-weather', 'Poison Scent', '[upkeep]');
-			                  if (this.isWeather('Poison Scent')) this.eachEvent('Weather');
-		                    },
-		                    onWeather: function (target) {
-                        let typeMod = this.clampIntRange(pokemon.runEffectiveness('Poison'), -6, 6);
-                                      if (pokemon.hasType('Steel') )
-                                      this.damage === 0;
-                                      elseif (pokemon.hasType('Poison') )
-                                      this.damage === 0;
-                                      else this.damage(pokemon.maxhp * Math.pow(2, typeMod) / 8);  
-		                            }
-				   },
-		                    onEnd: function () {
-			                   this.add('-weather', 'none');
-		                   },
- };
+		"poisonscent": {
+			effectType: 'Weather',
+			duration: 5,
+		},
+		onStart: function(battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectData.duration = 0;
+				this.add('-weather', 'Poison Scent', '[from] ability: ' + effect, '[of] ' + source);
+			} else {
+				this.add('-weather', 'Poison Scent');
+			}
+		},
+		onResidualOrder: 1,
+		onResidual: function() {
+			this.add('-weather', 'Poison Scent', '[upkeep]');
+			if (this.isWeather('Poison Scent')) this.eachEvent('Weather');
+		},
+		onWeather: function(target) {
+			let typeMod = this.clampIntRange(pokemon.runEffectiveness('Poison'), -6, 6);
+			if (pokemon.hasType('Steel'))
+				this.damage === 0;
+			elseif(pokemon.hasType('Poison'))
+			this.damage === 0;
+			else this.damage(pokemon.maxhp * Math.pow(2, typeMod) / 8);
+		}
+	},
+	onEnd: function() {
+		this.add('-weather', 'none');
+	},
+};
