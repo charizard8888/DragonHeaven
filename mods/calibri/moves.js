1b2a04c3d68be73068c843032f78bdd0c33be6ea
HiTechFlufi <zach.hartin@gmail.com>
2017-04-20 18:13:37 -0400

    moves.js
    commit to current PR
    
    -flufi

--- mods/calibri/moves.js
+++ mods/calibri/moves.js
@@ -24,88 +24,101 @@ sound: Has no effect on Pokemon with the Ability Soundproof.
 'use strict';
 
 exports.BattleMovedex = {
-    "mythicalaura": {
-          accuracy: true,
-          basePower: 0,
-          category: "Status",
-          desc: "Raises the user's accuracy by 1 stage and allows Dragon type moves to hit Fairy type Pokemon. This effect is canceled if Odor Sleuth, Foresight or Miracle Eye is used.",
-          shortDesc: "Raises the user's accuracy by 1; Dragon can hit Fairy types.",
-          id: "mythicalaura",
-          name: "Mythical Aura",
-          pp: 20,
-          priority: 0,
-          flags: {protect: 1, reflectable: 1, mirror: 1, authentic: 1},
-          volatileStatus: 'miracleeye',
-          onTryHit: function (target) {
-            if (target.volatiles['foresight']) return false;
-          },
-          effect: {
-            noCopy: true,
-            onStart: function (pokemon) {
-              this.add('-start', pokemon, 'Miracle Eye');
-            },
-            onNegateImmunity: function (pokemon, type) {
-              if (pokemon.hasType('Fairy') && type === 'Dragon') return false;
-            },
-            onModifyBoost: function (boosts) {
-              if (boosts.evasion && boosts.evasion > 0) {
-                boosts.evasion = 0;
-              }
-            },
-          },
-          secondary: {
-                      chance: 100,
-                      self: {
-                       boosts: {
-                        acc: 1,
-                },
-            },
-          },
-          target: "normal",
-          type: "Dragon",
-    },
-    "energygamble": {
-		accuracy: true,
-		basePower: 0,
-		category: "Special",
-		desc: "Doesn't check accuracy. Becomes either a 30 BP or 150 BP move when used. Locked Base Power of 200 when used as a Z-Move.",
-		shortDesc: "Becomes either 30 or 150 Base Power on use.",
-		id: "energygamble",
-		name: "Energy Gamble",
+    "eradicate": {
+		num: 1000,
+		accuracy: 65,
+		basePower: 145,
+		category: "Physical",
+		desc: "This move combines Ground in its type effectiveness against the target. Damage doubles and no accuracy check is done if the target has used Minimize while active.",
+		shortDesc: "Combines Ground in type effectiveness. 1/4 Recoil.",
+		id: "eradicate",
+		name: "Eradicate",
 		pp: 5,
+		flags: {contact: 1, protect: 1, mirror: 1, gravity: 1, distance: 1, nonsky: 1},
+	        recoil: [1, 4],
+		onEffectiveness: function (typeMod, type, move) {
+			return typeMod + this.getEffectiveness('Ground', type);
+		},
 		priority: 0,
-		flags: {protect: 1, mirror: 1, distance: 1},
-		onModifyMove: function (move, pokemon, target) {
-			let rand = this.random(10);
-			if (rand < 6) {
-				move.basePower = 30;
-			} else if (rand > 6) {
-				move.basePower = 150;
-			}
+		secondary: false,
+		target: "allAdjacent",
+		type: "Dragon",
+		zMovePower: 200,
+		contestType: "Cool",
+    },
+    "hitandrun": {
+		num: 1001,
+		accuracy: 90,
+		basePower: 95,
+		category: "Physical",
+		desc: "If this move is successful and the user has not fainted, the user switches out even if it is trapped and is replaced immediately by a selected party member. The user does not switch out if there are no unfainted party members, or if the target switched out using an Eject Button.",
+		shortDesc: "Forces user to switch out. 1/8 Recoil.",
+		id: "hitandrun",
+		isViable: true,
+		name: "Hit And Run",
+		pp: 20,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+	        recoil: [1, 8],
+	        selfSwitch: true,
+		secondary: false,
+		target: "normal",
+		type: "Normal",
+		zMovePower: 135,
+		contestType: "Cute",
+    },
+    "shieldstrike": {
+		num: 1002,
+		accuracy: false,
+		basePower: 75,
+		category: "Physical",
+		desc: "Doesn't make accuracy check; Guaranteed to hit. Protects the user from critical hits for 1 turn.",
+		shortDesc: "No accuracy check. Blocks crits for 1 turn.",
+		id: "shieldstrike",
+		name: "Shield Strike",
+		pp: 20,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		sideCondition: 'luckychant',
+		effect: {
+			duration: 1,
+			onStart: function (side) {
+				this.add('-sidestart', side, 'move: Lucky Chant'); // "The Lucky Chant shielded [side.name]'s team from critical hits!"
+			},
+			onCriticalHit: false,
+			onResidualOrder: 21,
+			onResidualSubOrder: 5,
+			onEnd: function (side) {
+				this.add('-sideend', side, 'move: Lucky Chant'); // "[side.name]'s team's Lucky Chant wore off!"
+			},
 		},
 		secondary: false,
 		target: "normal",
-		type: "Psychic",
-		zMovePower: 200,
-		contestType: "Cool",
-      },
-      "Tundra Sweep": {
-		accuracy: true,
-		basePower: 110,
+		type: "Fighting",
+		zMoveBoost: {evasion: 2},
+		contestType: "Tough",
+    },
+    "tundrasweep": {
+		num: 1003,
+		accuracy: 100,
+		basePower: 70,
 		category: "Special",
-		desc: "Hits all adjacent Pokemon, including allies in Doubles.",
-		shortDesc: "Hits all adjacent Pokemon.",
+		shortDesc: "Hits all Pokemon. 20% chance to freeze.",
 		id: "tundrasweep",
+		isViable: true,
 		name: "Tundra Sweep",
-		pp: 5,
+		pp: 10,
 		priority: 0,
-		flags: {protect: 1, mirror: 1, distance: 1},
-	        },
-		secondary: false,
+		flags: {protect: 1, mirror: 1},
+		secondary: {
+			chance: 20,
+			status: 'frz',
+		},
 		target: "allAdjacent",
 		type: "Ice",
-		zMovePower: 195,
-		contestType: "Cool",
+		zMovePower: 135,
+		contestType: "Beautiful",
 	},
     
 };
+	    
