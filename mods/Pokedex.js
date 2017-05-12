207706a1ca64f62a47497dffcb4875c5feab84eb
Ludicrousity <nathangtcooper@gmail.com>
2017-05-12 15:57:56 +0100

    Create Pokedex.js (#620)
    Could you put this into a folder called Eevee'd please.

---
+++ mods/Pokedex.js
@@ -0,0 +1,56 @@
+},
+  fletchinder: {
+		num: 662,
+		species: "Fletchinder",
+		types: ["Fire","Flying"],
+		baseStats: {hp: 62, atk: 73, def: 55, spa: 56, spd: 52, spe: 84},
+		abilities: {0: "Flame Body", H: "Gale Wings"},
+		heightm: 0.7,
+		weightkg: 16,
+		color: "Red",
+		prevo: "fletchling",
+		evos: ["talonflame","talonfey","talonwave","talonacid","talondrake","talonvolt"],
+		evoLevel: 17,
+		eggGroups: ["Flying"],
+  },
+    talonfey: {
+      num: 9001
+      species: "Talonfey"
+      types: ["Fairy","Flying"]
+      baseStats: {hp: 81, atk: 126, def: 71, spa: 69, spd: 74, spe: 78},
+      abilities: {0: "Dazzling", H: "Gale Wings"},
+      prevo: "fletchinder",
+  },
+    talonwave: {
+      num: 9002
+      species: "Talonwave"
+      types: ["Water","Flying"]
+      baseStats: {hp: 126, atk: 71, def: 78, spa: 81, spd: 74, spe: 69},
+      abilities: {0: "Water Absorb", H: "Gale Wings"},
+      prevo: "fletchinder",
+  },
+    talonacid: {
+      num: 9003
+      species: "Talonacid"
+      types: ["Poison","Flying"]
+      baseStats: {hp: 74, atk: 69, def: 126, spa: 78, spd: 81, spe: 71},
+      abilities: {0: "Poison Point", H: "Corrosion"},
+      prevo: "fletchinder",
+  },
+    talondrake: {
+      num: 9004
+      species: "Talondrake"
+      types: ["Dragon","Flying"]
+      baseStats: {hp: 78, atk: 81, def: 74, spa: 126, spd: 69, spe: 71},
+      abilities: {0: "Tough Claws", H: "Gale Wings"},
+      prevo: "fletchinder",
+  },
+    talonvolt: {
+      num: 9005
+      species: "Talonvolt"
+      types: ["Electric","Flying"]
+      baseStats: {hp: 69, atk: 74, def: 71, spa: 126, spd: 78, spe: 81},
+      abilities: {0: "Static", H: "Gale Wings"},
+      prevo: "fletchinder",
+  },
+};
