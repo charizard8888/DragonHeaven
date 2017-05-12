6711a8561b4d13a4aaae56c07b7a4c33b90d6ec3
EchoGaia <maxime.rodrigues.pro@gmail.com>
2017-05-12 21:35:39 +0200

    Update pokedex.js (#622)
    Fixes :)

--- mods/eeveed/pokedex.js
+++ mods/eeveed/pokedex.js
@@ -1,4 +1,6 @@
-},
+'use strict';
+
+ exports.BattlePokedex = {
   fletchinder: {
 		num: 662,
 		species: "Fletchinder",
@@ -13,44 +15,44 @@
 		evoLevel: 17,
 		eggGroups: ["Flying"],
   },
-    talonfey: {
-      num: 9001
-      species: "Talonfey"
-      types: ["Fairy","Flying"]
-      baseStats: {hp: 81, atk: 126, def: 71, spa: 69, spd: 74, spe: 78},
-      abilities: {0: "Dazzling", H: "Gale Wings"},
-      prevo: "fletchinder",
+  talonfey: {
+      		num: 9001,
+      		species: "Talonfey",
+      		types: ["Fairy","Flying"],
+      		baseStats: {hp: 81, atk: 126, def: 71, spa: 69, spd: 74, spe: 78},
+      		abilities: {0: "Dazzling", H: "Gale Wings"},
+      		prevo: "fletchinder",
   },
-    talonwave: {
-      num: 9002
-      species: "Talonwave"
-      types: ["Water","Flying"]
-      baseStats: {hp: 126, atk: 71, def: 78, spa: 81, spd: 74, spe: 69},
-      abilities: {0: "Water Absorb", H: "Gale Wings"},
-      prevo: "fletchinder",
+  talonwave: {
+      		num: 9002,
+      		species: "Talonwave",
+      		types: ["Water","Flying"],
+      		baseStats: {hp: 126, atk: 71, def: 78, spa: 81, spd: 74, spe: 69},
+      		abilities: {0: "Water Absorb", H: "Gale Wings"},
+      		prevo: "fletchinder",
   },
-    talonacid: {
-      num: 9003
-      species: "Talonacid"
-      types: ["Poison","Flying"]
-      baseStats: {hp: 74, atk: 69, def: 126, spa: 78, spd: 81, spe: 71},
-      abilities: {0: "Poison Point", H: "Corrosion"},
-      prevo: "fletchinder",
+  talonacid: {
+      		num: 9003,
+      		species: "Talonacid",
+     		types: ["Poison","Flying"],
+      		baseStats: {hp: 74, atk: 69, def: 126, spa: 78, spd: 81, spe: 71},
+      		abilities: {0: "Poison Point", H: "Corrosion"},
+      		prevo: "fletchinder",
   },
-    talondrake: {
-      num: 9004
-      species: "Talondrake"
-      types: ["Dragon","Flying"]
-      baseStats: {hp: 78, atk: 81, def: 74, spa: 126, spd: 69, spe: 71},
-      abilities: {0: "Tough Claws", H: "Gale Wings"},
-      prevo: "fletchinder",
+  talondrake: {
+      		num: 9004,
+      		species: "Talondrake",
+     		types: ["Dragon","Flying"],
+      		baseStats: {hp: 78, atk: 81, def: 74, spa: 126, spd: 69, spe: 71},
+      		abilities: {0: "Tough Claws", H: "Gale Wings"},
+      		prevo: "fletchinder",
   },
-    talonvolt: {
-      num: 9005
-      species: "Talonvolt"
-      types: ["Electric","Flying"]
-      baseStats: {hp: 69, atk: 74, def: 71, spa: 126, spd: 78, spe: 81},
-      abilities: {0: "Static", H: "Gale Wings"},
-      prevo: "fletchinder",
+  talonvolt: {
+      		num: 9005,
+      		species: "Talonvolt",
+      		types: ["Electric","Flying"],
+      		baseStats: {hp: 69, atk: 74, def: 71, spa: 126, spd: 78, spe: 81},
+      		abilities: {0: "Static", H: "Gale Wings"},
+      		prevo: "fletchinder",
   },
 };
