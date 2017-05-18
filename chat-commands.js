71451cdd92ec153cd8464429af899888448e65b4
Spandamn <spandan.punwatkar@gmail.com>
2017-05-18 21:37:56 +0530

    Update

--- chat-commands.js
+++ chat-commands.js
@@ -2608,6 +2608,7 @@ exports.commands = {
 				Chat.uncacheTree('./chat');
 				delete require.cache[require.resolve('./chat-commands')];
 				delete require.cache[require.resolve('./chat-plugins/info')];
+				delete require.cache[require.resolve('./chat-plugins/github')];
 				global.Chat = require('./chat');
 
 				let runningTournaments = Tournaments.tournaments;
