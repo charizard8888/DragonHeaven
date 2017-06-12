e5e66611bb27751046490bd581c79c1e0abdebc9
charizard8888 <omkumr082001@outlook.com>
2017-06-12 16:32:15 +0530

    Create moves.js

--- mods/dhssb/moves.js
+++ mods/dhssb/moves.js
@@ -55,31 +55,6 @@ exports.BattleMovedex = {
 		zMoveBoost: {evasion: 1},
 		contestType: "Clever",
 	},
-	"soontm": {
-		accuracy: 80,
-		basePower: 140,
-		category: "Physical",
-		desc: "Can hit Ghost Types.",
-		shortDesc: "Can hit Ghost Types.",
-		id: "soontm",
-		isViable: true,
-		name: "Soon TM",
-		pp: 5,
-		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1, gravity: 1},
-		onPrepareHit: function (target, source) {
-			this.attrLastMove('[still]');
-			this.add('-anim', source, "High Jump Kick", target);
-		},
-		onEffectiveness: function (typeMod, type) {
-			if (type === 'Ghost') return 0;
-		},
-		secondary: false,
-		target: "normal",
-		type: "Fighting",
-		zMovePower: 195,
-		contestType: "Cool",
-	},
 	"corruptaura": {
 		accuracy: 80,
 		basePower: 140,
