803b71eda78940256290f3d9f518baa038f0acf0
Spandamn <spandan.punwatkar@gmail.com>
2017-06-24 15:16:55 +0530

    Update

--- mods/chimera1v1/scripts.js
+++ mods/chimera1v1/scripts.js
@@ -62,7 +62,7 @@ exports.BattleScripts = {
 			break;
 		case 'team': {
 			decision.side.pokemon.splice(decision.index, 0, decision.pokemon);
-			decision.side.choice.count = 6;
+			decision.side.pokemon[5].baseAbility = 'illusion';
 			decision.pokemon.position = decision.index;
 			// we return here because the update event would crash since there are no active pokemon yet
 			return;
