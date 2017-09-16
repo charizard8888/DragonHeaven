0d07b6193d8ab88090ada2f681e057928bcac5b9
charizard8888 <omkumr082001@outlook.com>
2017-09-16 12:17:58 +0530

    Create formats-data.js

---
+++ mods/fe/formats-data.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.BattleFormatsData = {
+
+swampamar: {
+		randomBattleMoves: ["fireblast", "clangingscales", "defog", "roost"],
+		tier: "FE",
+	},
+  swampamarmega: {
+		randomBattleMoves: ["dragondance", "flareblitz", "dragonclaw", "earthquake", "roost", "willowisp"],
+		randomDoubleBattleMoves: ["dragondance", "flareblitz", "dragonclaw", "earthquake", "rockslide", "roost", "substitute"],
+		requiredItem: "Swampamarite",
+		tier: "FE",
+	},
+  };
