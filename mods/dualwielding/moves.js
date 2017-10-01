5f1e51e4ca6dd2a5ecedd36aec8558b4b22fd1c9
Spandamn <spandan.punwatkar@gmail.com>
2017-10-01 15:01:48 +0530

    Dual Wielding fixes

--- mods/dualwielding/moves.js
+++ mods/dualwielding/moves.js
@@ -10,19 +10,21 @@ exports.BattleMovedex = {
 			}
 			return move.basePower;
 		},
-	},/*
+	},
+	"entrainment": {
+		inherit: true,
+		onTryHit: function (target, source) {
+			return false;
+		},
+	},
+	"gastroacid": {
+		inherit: true,
+		onTryHit: function (target, source) {
+			return false;
+		},
+	},
 	"incinerate": {
-		num: 510,
-		accuracy: 100,
-		basePower: 60,
-		category: "Special",
-		desc: "The target loses its held item if it is a Berry or a Gem. This move cannot cause Pokemon with the Ability Sticky Hold to lose their held item. Items lost to this move cannot be regained with Recycle or the Ability Harvest.",
-		shortDesc: "Destroys the foe(s) Berry/Gem.",
-		id: "incinerate",
-		name: "Incinerate",
-		pp: 15,
-		priority: 0,
-		flags: {protect: 1, mirror: 1},
+		inherit: true,
 		onHit: function (pokemon, source) {
 			let item = pokemon.getItem(), item2 = pokemon.getAbility();
 			if ((item.isBerry || item.isGem) && pokemon.takeItem(source)) {
@@ -32,12 +34,7 @@ exports.BattleMovedex = {
 				this.add('-enditem', pokemon, item2.name, '[from] move: Incinerate');
 			}
 		},
-		secondary: false,
-		target: "allAdjacentFoes",
-		type: "Fire",
-		zMovePower: 120,
-		contestType: "Tough",
-	},*/
+	},
 	"judgment": {
 		inherit: true,
 		onModifyMove: function (move, pokemon) {
@@ -48,50 +45,7 @@ exports.BattleMovedex = {
 				move.type = item2.onPlate;
 			}
 		},
-	},/*
-	"knockoff": {
-		num: 282,
-		accuracy: 100,
-		basePower: 65,
-		category: "Physical",
-		desc: "If the target is holding an item that can be removed from it, ignoring the Ability Sticky Hold, this move's power is multiplied by 1.5. If the user has not fainted, the target loses its held item. This move cannot remove Z-Crystals, cause Pokemon with the Ability Sticky Hold to lose their held item, cause Pokemon that can Mega Evolve to lose the Mega Stone for their species, or cause a Kyogre, a Groudon, a Giratina, an Arceus, a Genesect, or a Silvally to lose their Blue Orb, Red Orb, Griseous Orb, Plate, Drive, or Memory respectively. Items lost to this move cannot be regained with Recycle or the Ability Harvest.",
-		shortDesc: "1.5x damage if foe holds an item. Removes item.",
-		id: "knockoff",
-		isViable: true,
-		name: "Knock Off",
-		pp: 20,
-		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1},
-		onBasePowerPriority: 4,
-		onBasePower: function (basePower, source, target, move) {
-			let item = target.getItem(), item2 = target.getAbility();
-			if (!this.singleEvent('TakeItem', item, {id: item.id, 'target': target}, target, source, move, item) && !this.singleEvent('TakeItem', item2, {id: item2.id, 'target': target}, target, source, move, item2)) return;
-			let boost = 1;
-			[item, item2].forEach (i => {
-				if (i.id) boost *= 1.5;
-			});
-			if (item2.id || item.id) {
-				return this.chainModify(boost);
-			}
-		},
-		onAfterHit: function (target, source) {
-			if (source.hp) {
-				let item = target.takeItem();
-				if (item) {
-					this.add('-enditem', target, item.name, '[from] move: Knock Off', '[of] ' + source);
-				}
-				let item2 = target.takeItem();
-				if (item2) {
-					this.add('-enditem', target, item2.name, '[from] move: Knock Off', '[of] ' + source);
-				}
-			}
-		},
-		secondary: false,
-		target: "normal",
-		type: "Dark",
-		zMovePower: 120,
-		contestType: "Clever",
-	},*/
+	},
 	"recycle": {
 		inherit: true,
 		onHit: function (pokemon) {
@@ -100,6 +54,18 @@ exports.BattleMovedex = {
 			this.add('-item', pokemon, pokemon.getItem(), '[from] move: Recycle');
 			pokemon.setItem(pokemon.lastItem2, pokemon, {item2: true});
 			this.add('-item', pokemon, pokemon.getAbility(), '[from] move: Recycle');
+		},
+	},
+	"roleplay": {
+		inherit: true,
+		onTryHit: function (target, source) {
+			return false;
+		},
+	},
+	"simplebeam": {
+		inherit: true,
+		onTryHit: function (target, source) {
+			return false;
 		},
 	},
 	"skillswap": {
