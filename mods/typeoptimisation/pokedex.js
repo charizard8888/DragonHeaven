8b20c3944a81fedd115546246e07da9a7d034de1
charizard8888 <omkumr082001@outlook.com>
2017-10-05 17:14:12 +0530

    Update pokedex.js

--- mods/typeoptimisation/pokedex.js
+++ mods/typeoptimisation/pokedex.js
@@ -2,18 +2,14 @@
 
 exports.BattlePokedex = {
 lycanroc: {
-		num: 745,
-		species: "Lycanroc",
-		baseForme: "Midday",
-		types: ["Rock"],
+		inherit: true,
+		types: ["Rock", "Normal"],
 		baseStats: {hp: 75, atk: 120, def: 60, spa: 55, spd: 60, spe: 117},
-		abilities: {0: "Keen Eye", 1: "Sand Rush", H: "Steadfast"},
-		heightm: 0.8,
-		weightkg: 25,
-		color: "Brown",
-		prevo: "rockruff",
-		evoLevel: 25,
-		eggGroups: ["Field"],
-		otherFormes: ["lycanrocmidnight"],
+	},
+	emboar: {
+		inherit: true,
+		types: ["Rock", "Fire"],
+		baseStats: {hp: 95, atk: 138, def: 65, spa: 110, spd: 65, spe: 97},
+		abilities: {0: "Reckless", 1: "Dazzling", H: "Magic Guard"},
 	},
   };
