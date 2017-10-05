52fa970526abd65e320c9c5a7222fcd73f298d71
charizard8888 <omkumr082001@outlook.com>
2017-10-05 17:06:38 +0530

    Create pokedex.js

---
+++ mods/typeoptimisation/pokedex.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.BattlePokedex = {
+lycanroc: {
+		num: 745,
+		species: "Lycanroc",
+		baseForme: "Midday",
+		types: ["Rock"],
+		baseStats: {hp: 75, atk: 120, def: 60, spa: 55, spd: 60, spe: 117},
+		abilities: {0: "Keen Eye", 1: "Sand Rush", H: "Steadfast"},
+		heightm: 0.8,
+		weightkg: 25,
+		color: "Brown",
+		prevo: "rockruff",
+		evoLevel: 25,
+		eggGroups: ["Field"],
+		otherFormes: ["lycanrocmidnight"],
+	},
+  };
