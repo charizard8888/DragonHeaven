740cf8fd209a6f2b6cbd90f6ed9838a465b5dd84
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-10-15 16:03:04 +0530

    Upd8 main (#712)
    * Trivia: make searching results scroll (#4030)
    
    * Revert "Trivia: make searching results scroll (#4030)" (#4031)
    
    This reverts commit 2aa8f12ea63e53cc1f6c8882dea86988dbf0bb3b.
    
    * Scavengers: Remove the temporary warning on commas (#4032)
    
    * Add new OM of the Month
    
    * Add new RoA Spotlight
    
    * Add 1v1 suspect test
    
    * Sketchmons: Unban Dugtrio
    
    * Gitignore all chat plugins ending in -private
    
    Closes #4033
    
    * Datacenters: Add a range (#4034)
    
    * Update Encore
    
    * Update Ultimate Z
    
    * Remove RU suspect test
    
    * Ultimate Z: Ban Hold Hands
    
    * Fix typo in lockdown message
    
    * Wifi: change syntax error message to use pipes instead of commas
    
    * Wifi: fix typo
    
    * Wifi: add help for /giveaway count
    
    * Update Encore & Fake Out move animations (#4035)
    
    * BH: Ban Primal Groudon (#4037)
    
    * Pass the active move when modifying stats (#4038)
    
    * Move Sharpedo to BL2
    
    * Improve Ingrain's short description (#4040)
    
    * Doubles UU: update mega stone bans
    
    * Trivia: Delete questions by ID (#4042)
    
    * Update NU
    
    * Ultimate Z: Add new bans
    
    * Fix Reflect Type vs. Pokemon with the "???" type (#4039)
    
    * Implement October tier shifts
    
    * Update Sketchmons
    
    * Guzzlord is NU
    
    * Modlog: fix result count display being off by one (#4044)
    
    * Properly format the word Pokémon in datasearch (#4043)
    
    * Gen II: Hardcode Beat Up stat replacement
    
    Fixes the `ModifyAtk` bug and brings it in line with every other effect in Gen 2, plus actually implements the level part of Beat Up.
    
    * Fix whitespace in HF chat plugin
    
    * Support tracking rating in uploaded replays
    
    * Fix Gen 2 paralysis Spe
    
    * Update dependencies
    
    Mocha 4 has a weird issue where it doesn't terminate, so we can't use
    it for now.
    
    * Force linebreaks at the ends of files
    
    * Update Mocha dependency
    
    * Implement tiebreaker
    
    This is the tiebreaker used for timeouts in Gen 6-7 (I think), but it's
    not currently used anywhere.
    
    * Update misc dependencies
    
    * Make eslint rules stricter
    
    * Remove PU suspect test
    
    * Gen II: Fix Beat Up again
    
    * Add Monotype suspect test (#4046)
    
    * Random Doubles: Update Necrozma (#4047)
    
    * Roomunban: Clarify not banned error message (#4048)
    
    * Implement /ionext
    
    Sets your next battle as invite-only.
    
    * Rename user.ioNext -> user.inviteOnlyNextBattle
    
    * README: Update David-DM devDependencies link
    
    * Don't send people with HTTPS pages with /smogdex (#4049)
    
    People have reported getting errors like https://i.gyazo.com/778739472cf16b75377bef1c506a493d.png when using `/smogdex`. While this is a smogon-side issue, this is a temporary fix to that error.
    
    * Don't send people to HTTPS links on smogon (#4050)
    
    Smogon doesn't support HTTPS.
    
    * Standardize on/off commands
    
    * Gen III: Fix Enigma Berry
    
    * Wifi: Fix formatting in popups
    
    * Remove AAA suspect test
    
    * /ds<N> single result details should be for gen<N> (#4051)
    
    * Update CONTRIBUTING.md
    
    V8 6.1 and Node 8.7 fix performance for basically everything I care about.
    
    * Add new event Pokemon
    
    * Correct confusion prevention messages
    
    * tcg: update to use https, fix error with bulba (#4053)
    
    seems like bulbapedia requires https now, which was breaking ptcg requests
    
    * Remove UU suspect test
    
    * Datacenters: Add a range (#4054)
    
    * Modchat: Remove unnecessary curly brackets (#4057)
    
    * Fix ending hunts within a parent game (#4056)
    
    * Scavenger games: fix timer (#4058)
    
    - return the value of the timer duration in a scav game so it does not end up as ``undefined``

--- mods/ultimatez/scripts.js
+++ mods/ultimatez/scripts.js
@@ -1,80 +1,6 @@
 'use strict';
 
 exports.BattleScripts = {
-	runMove: function (move, pokemon, targetLoc, sourceEffect, zMove, externalMove) {
-		let target = this.getTarget(pokemon, zMove || move, targetLoc);
-		if (!sourceEffect && toId(move) !== 'struggle' && !zMove) {
-			let changedMove = this.runEvent('OverrideDecision', pokemon, target, move);
-			if (changedMove && changedMove !== true) {
-				move = changedMove;
-				target = null;
-			}
-		}
-		let baseMove = this.getMove(move);
-		move = zMove ? this.getZMoveCopy(move, pokemon) : baseMove;
-		if (!target && target !== false) target = this.resolveTarget(pokemon, move);
-
-		// copy the priority for Quick Guard
-		if (zMove) move.priority = baseMove.priority;
-		move.isExternal = externalMove;
-
-		this.setActiveMove(move, pokemon, target);
-
-		/* if (pokemon.moveThisTurn) {
-			// THIS IS PURELY A SANITY CHECK
-			// DO NOT TAKE ADVANTAGE OF THIS TO PREVENT A POKEMON FROM MOVING;
-			// USE this.cancelMove INSTEAD
-			this.debug('' + pokemon.id + ' INCONSISTENT STATE, ALREADY MOVED: ' + pokemon.moveThisTurn);
-			this.clearActiveMove(true);
-			return;
-		} */
-		if (!this.runEvent('BeforeMove', pokemon, target, move)) {
-			this.runEvent('MoveAborted', pokemon, target, move);
-			// Prevent Pursuit from running again against a slower U-turn/Volt Switch/Parting Shot
-			pokemon.moveThisTurn = true;
-			this.clearActiveMove(true);
-			return;
-		}
-		if (move.beforeMoveCallback) {
-			if (move.beforeMoveCallback.call(this, pokemon, target, move)) {
-				this.clearActiveMove(true);
-				return;
-			}
-		}
-		pokemon.lastDamage = 0;
-		let lockedMove;
-		if (!externalMove) {
-			lockedMove = this.runEvent('LockMove', pokemon);
-			if (lockedMove === true) lockedMove = false;
-			if (!lockedMove) {
-				if (!pokemon.deductPP(baseMove, null, target) && (move.id !== 'struggle')) {
-					this.add('cant', pokemon, 'nopp', move);
-					let gameConsole = [null, 'Game Boy', 'Game Boy', 'Game Boy Advance', 'DS', 'DS'][this.gen] || '3DS';
-					this.add('-hint', "This is not a bug, this is really how it works on the " + gameConsole + "; try it yourself if you don't believe us.");
-					this.clearActiveMove(true);
-					return;
-				}
-			} else {
-				sourceEffect = this.getEffect('lockedmove');
-			}
-			pokemon.moveUsed(move, targetLoc);
-		}
-
-		// Dancer Petal Dance hack
-		// TODO: implement properly
-		let noLock = externalMove && !pokemon.volatiles.lockedmove;
-
-		if (zMove) {
-			if (pokemon.illusion) {
-				this.singleEvent('End', this.getAbility('Illusion'), pokemon.abilityData, pokemon);
-			}
-			this.add('-zpower', pokemon);
-		}
-		this.useMove(baseMove, pokemon, target, sourceEffect, zMove);
-		this.singleEvent('AfterMove', move, null, pokemon, target, move);
-		this.runEvent('AfterMove', pokemon, target, move);
-		if (noLock && pokemon.volatiles.lockedmove) delete pokemon.volatiles.lockedmove;
-	},
 	getZMove: function (move, pokemon, skipChecks) {
 		let item = pokemon.getItem();
 		if (!skipChecks) {
@@ -84,32 +10,20 @@ exports.BattleScripts = {
 			if (!moveData || !moveData.pp) return; // Draining the PP of the base move prevents the corresponding Z-move from being used.
 		}
 
-		if (item.zMoveFrom) {
-			if (move.name === item.zMoveFrom) return item.zMove;
-		} else if (item.zMove === true) {
-			if (move.category === "Status") {
-				return move.name;
-			} else if (move.zMovePower) {
-				return this.zMoveTable[item.zMoveType];
-			}
-		}
+		if (move.category === 'Status') return move.name;
+		if (item.zMoveFrom) return move.name === item.zMoveFrom && item.zMove;
+		return move.zMovePower && this.zMoveTable[item.zMoveType];
 	},
 	getZMoveCopy: function (move, pokemon) {
 		move = this.getMove(move);
-		let zMove;
-		if (pokemon) {
-			let item = pokemon.getItem();
-			if (move.name === item.zMoveFrom) {
-				return this.getMoveCopy(item.zMove);
-			}
-		}
-
 		if (move.category === 'Status') {
-			zMove = this.getMoveCopy(move);
+			let zMove = this.getMoveCopy(move);
 			zMove.isZ = true;
 			return zMove;
 		}
-		zMove = this.getMoveCopy(this.zMoveTable[pokemon.getItem().zMoveType]);
+		let item = pokemon.getItem();
+		if (item.zMoveFrom) return this.getMoveCopy(item.zMove);
+		let zMove = this.getMoveCopy(this.zMoveTable[item.zMoveType]);
 		zMove.basePower = move.zMovePower;
 		zMove.category = move.category;
 		return zMove;
