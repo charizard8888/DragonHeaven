8a95f3e2539d861d75535e5a9357d87731a02b8d
charizard8888 <omkumr082001@outlook.com>
2018-01-17 12:40:27 +0530

    Create pokedex.js

---
+++ mods/usv/pokedex.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.BattlePokedex = {
+	/* For pokemon with two abilities use
+	   abilities: {0: "Ability1Name", H: "Ability2Name"},
+	   For a pokemon with three abilities use
+	   abilities: {0: "Ability1Name", 1: "Ability2Name", H: "Ability3Name"}, */
+	
+  gumshoosub: {
+		num: 735,
+		species: "Gumshoos-Ultra Beast",
+		baseSpecies: "Gumshoos",
+		forme: "Ultra Beast",
+		formeLetter: "UB",
+		types: ["Normal"],
+		baseStats: {hp: 211, atk: 107, def: 97, spa: 17, spd: 67, spe: 71},
+		abilities: {0: "Beast Boost", H:"Stakeout"},
+	},
+  
+  };
