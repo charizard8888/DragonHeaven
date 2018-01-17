7e4eec5ff2275ecb4b9ff6304875fa48c839fc98
charizard8888 <omkumr082001@outlook.com>
2018-01-17 16:36:06 +0530

    Update learnsets.js

--- mods/eeveed/learnsets.js
+++ mods/eeveed/learnsets.js
@@ -1,54 +1,68 @@
 'use strict';
 
 exports.BattleLearnsets = {
-        talonfey: {
-            learnset: {
-                fairywind: ["7L1"],
-                disarmingvoice: ["7L1"],
-                dazzlinggleam: ["7L1"],
-                playrough: ["7L1"],
-                partingshot: ["7L1"],
-                superpower: ["7L1"],
-            }
-        },
-        talonwave: {
-            learnset: {
-                waterfall: ["7L1"],
-                surf: ["7L1"],
-                aquatail: ["7L1"],
-                scald: ["7L1"],
-                hydropump: ["7L1"],
-                waterfun: ["7L1"],
-                bubblegeam: ["7L1"],
-                airslash: ["7L1"],
-                hurricane: ["7L1"],
-            }
-        },
-        talonacid: {
-            learnset: {
-                sludgewave: ["7L1"],
-                sludgebomb: ["7L1"],
-                poisonjab: ["7L1"],
-                crosspoison: ["7L1"],
-                venoshock: ["7L1"],
-                sludge: ["7L1"],
-                clearsmog: ["7L1"],
-                acid: ["7L1"],
-                acidspray: ["7L1"],
-                poisonsting: ["7L1"],
-                toxicspikes: ["7L1"],
-                venomdrench: ["7L1"],
-            }
-        },
-        talondrake: {
-            learnset: {
-                dragonclaw: ["7L1"],
-                outrage: ["7L1"],
-                shadowclaw: ["7L1"],
-                dragonpulse: ["7L1"],
-                dracometeor: ["7L1"],
-                hurricane: ["7L1"],
-                airslash: ["7L1"],
-                dragondance: ["7L1"],
-                honeclaws: ["7L1"],
-            };
+    talonfey: {
+        learnset: {
+            fairywind: ["7L1"],
+            disarmingvoice: ["7L1"],
+            dazzlinggleam: ["7L1"],
+            playrough: ["7L1"],
+            partingshot: ["7L1"],
+            superpower: ["7L1"],
+        }
+    },
+    talonwave: {
+        learnset: {
+            waterfall: ["7L1"],
+            surf: ["7L1"],
+            aquatail: ["7L1"],
+            scald: ["7L1"],
+            hydropump: ["7L1"],
+            waterfun: ["7L1"],
+            bubblegeam: ["7L1"],
+            airslash: ["7L1"],
+            hurricane: ["7L1"],
+        }
+    },
+    talonacid: {
+        learnset: {
+            sludgewave: ["7L1"],
+            sludgebomb: ["7L1"],
+            poisonjab: ["7L1"],
+            crosspoison: ["7L1"],
+            venoshock: ["7L1"],
+            sludge: ["7L1"],
+            clearsmog: ["7L1"],
+            acid: ["7L1"],
+            acidspray: ["7L1"],
+            poisonsting: ["7L1"],
+            toxicspikes: ["7L1"],
+            venomdrench: ["7L1"],
+        }
+    },
+    talondrake: {
+        learnset: {
+            dragonclaw: ["7L1"],
+            outrage: ["7L1"],
+            shadowclaw: ["7L1"],
+            dragonpulse: ["7L1"],
+            dracometeor: ["7L1"],
+            hurricane: ["7L1"],
+            airslash: ["7L1"],
+            dragondance: ["7L1"],
+            honeclaws: ["7L1"],
+        }
+    },
+    talonvolt: {
+        learnset: {
+            dishcarge: ["7L1"],
+            hurricane: ["7L1"],
+            thunderbolt: ["7L1"],
+            voltswitch: ["7L1"],
+            thunderwave: ["7L1"],
+            chargebeam: ["7L1"],
+            zapcannon: ["7L1"],
+            focusblast: ["7L1"],
+        }
+    },
+};
