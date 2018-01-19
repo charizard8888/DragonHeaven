d902519f4b1e392b2a341d587019148215d3250f
charizard8888 <omkumr082001@outlook.com>
2018-01-19 12:56:21 +0530

    Add Alola Formes

---
+++ mods/alola/pokedex.js
@@ -0,0 +1,55 @@
+'use strict';
+
+exports.BattlePokedex = {
+    pidgeyalola: {
+        num: 16,
+        species: "Pidgey-Alola",
+        baseSpecies: "Pidgey",
+        forme: "Alola",
+        formeLetter: "A",
+        types: ["Ground", "Ghost"],
+        baseStats: {
+            hp: 40,
+            atk: 40,
+            def: 40,
+            spa: 45,
+            spd: 35,
+            spe: 51
+        },
+        abilities: {0: "Cursed Body", 1: "Overcoat", H: "Serene Grace"},
+    },
+        pidgeottoalola: {
+        num: 16,
+        species: "Pidgeotto-Alola",
+        baseSpecies: "Pidgeotto",
+        forme: "Alola",
+        formeLetter: "A",
+        types: ["Ground", "Ghost"],
+        baseStats: {
+            hp: 63,
+            atk: 60,
+            def: 55,
+            spa: 60,
+            spd: 50,
+            spe: 61
+        },
+        abilities: {0: "Cursed Body", 1: "Overcoat", H: "Serene Grace"},
+    },
+     pidgeotalola: {
+        num: 16,
+        species: "Pidgeot-Alola",
+        baseSpecies: "Pidgeot",
+        forme: "Alola",
+        formeLetter: "A",
+        types: ["Ground", "Ghost"],
+        baseStats: {
+            hp: 80,
+            atk: 80,
+            def: 75,
+            spa: 83,
+            spd: 70,
+            spe: 91
+        },
+        abilities: {0: "Cursed Body", 1: "Overcoat", H: "Serene Grace"},
+    },
+    };
