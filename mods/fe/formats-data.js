3ddca2ad0a83101beea4f490c84b80d6b1441878
charizard8888 <omkumr082001@outlook.com>
2018-01-23 16:43:30 +0530

    Update formats-data.js

--- mods/fe/formats-data.js
+++ mods/fe/formats-data.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.BattleFormatsData = {
+exports.BattleFormatsData = { //
 
 swampamar: {
 		randomBattleMoves: ["dragondance", "flareblitz", "dragonclaw", "earthquake", "roost", "willowisp"],
@@ -9,6 +9,15 @@ swampamar: {
  swampamarmega: {
 		randomBattleMoves: ["dragondance", "flareblitz", "dragonclaw", "earthquake", "roost", "willowisp"],
 		requiredItem: "Swampamarite",
+		tier: "OU",
+	},
+	aerodako: {
+		randomBattleMoves: ["dragondance", "flareblitz", "dragonclaw", "earthquake", "roost", "willowisp"],
+		tier: "OU",
+	},
+	aerodakomega: {
+		randomBattleMoves: ["dragondance", "flareblitz", "dragonclaw", "earthquake", "roost", "willowisp"],
+		requiredItem: "Aerodakoite",
 		tier: "OU",
 	},
  };
