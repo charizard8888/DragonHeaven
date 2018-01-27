e91353b357259a48c9b9628bf0f782f90263d2fe
charizard8888 <omkumr082001@outlook.com>
2018-01-27 21:05:25 +0530

    Update moves.js

--- mods/eternal/moves.js
+++ mods/eternal/moves.js
@@ -788,7 +788,7 @@ Z-Move Effect: Does a 25BP Z-Move for all 8 attacks. (E.g, Hydro Vortex -> Gigav
 		zMovePower: 160,
 		contestType: "Beautiful",
 	},
-		"spikestorm": {
+		/*"spikestorm": {
 		accuracy: 90,
 		basePower: 20,
 		category: "Physical",
@@ -811,7 +811,7 @@ Z-Move Effect: Does a 25BP Z-Move for all 8 attacks. (E.g, Hydro Vortex -> Gigav
 		type: "Ground",
 		zMovePower: 80,
 		contestType: "Tough",
-	},
+	},*/
 	"metalliccharge": {
 		accuracy: 100,
 		basePower: 160,
