aa117f26302566cb7dd05d5fbffce8605196ebb4
charizard8888 <omkumr082001@outlook.com>
2018-01-28 16:50:05 +0530

    Create pokedex.js

---
+++ mods/sylvemons/pokedex.js
@@ -0,0 +1,45 @@
+'use strict';
+
+exports.BattlePokedex = {
+
+    torterra: {
+        inherit: true,
+        types: ["Grass", "Rock"],
+    },
+    absol: {
+        inherit: true,
+        types: ["Dark", "Fairy"],
+    },
+    absolmega: {
+        inherit: true,
+        types: ["Dark", "Fairy"],
+    },
+    blissey: {
+        inherit: true,
+        types: ["Normal", "Fairy"],
+    },
+    cresselia: {
+        inherit: true,
+        types: ["Psychic", "Fairy"],
+    },
+    Samurott: {
+        inherit: true,
+        types: ["Water", "Fighting"],
+    },
+    musharna: {
+        inherit: true,
+        types: ["Psychic", "Fairy"],
+    },
+    spritzee: {
+        inherit: true,
+        types: ["Fairy", "Poison"],
+    },
+    stakataka: {
+        inherit: true,
+        types: ["Ghost", "Steel"],
+    },
+    staraptor: {
+        inherit: true,
+        types: ["Fighting", "Flying"],
+    },
+};
