4d0db28535ee79f4b8bfe914f8675db5f406587c
charizard8888 <omkumr082001@outlook.com>
2018-01-30 14:16:51 +0530

    Create formats-data.js

---
+++ mods/choonmons/formats-data.js
@@ -0,0 +1,16 @@
+use strict';
+
+exports.BattleFormatsData = { //
+pikachulibre: {
+		randomBattleMoves: ["volttackle", "knockoff", "extremespeed", "imprisonedstrike"],
+		tier: "OU",
+	},
+porygonz: {
+		randomBattleMoves: ["thunderbolt", "shadowball", "icebeam", "psyshock"],
+		tier: "OU",
+	},
+  gastrodon: {
+		randomBattleMoves: ["scald", "earthpower", "recover", "toxic"],
+		tier: "OU",
+	},
+};
