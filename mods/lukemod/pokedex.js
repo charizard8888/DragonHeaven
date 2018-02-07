594fda050babb934e340c850da3888f4140964e6
charizard8888 <omkumr082001@outlook.com>
2018-02-08 00:59:25 +0530

    Update pokedex.js

--- mods/lukemod/pokedex.js
+++ mods/lukemod/pokedex.js
@@ -11,7 +11,22 @@ abilities: {0: "Illuminate", 1: "Effect Spore", H: "Triage"},
 },
 gastly: {
 inherit: true,
-types: "Ghost", "Poison", "luke"},
+types: ["Ghost", "Poison", "luke"],
 abilities: {0: "Cursed Body"},
+},
+haunter: {
+inherit: true,
+types: ["Ghost", "Poison", "luke"],
+abilities: {0: "Cursed Body"},
+},
+koffing: {
+inherit: true,
+types: ["Poison", "Fire", "luke"],
+abilities: {0: "Flash Fire", "Aftermath"},
+},
+weezing: {
+inherit: true,
+types: ["Poison", "Fire", "luke"],
+abilities: {0: "Flash Fire", "Aftermath"},
 },
 };
