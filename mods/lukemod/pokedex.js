a55bfe37b410f71455a4c2be95e1eadab9841cce
charizard8888 <omkumr082001@outlook.com>
2018-02-08 00:54:45 +0530

    Update pokedex.js

--- mods/lukemod/pokedex.js
+++ mods/lukemod/pokedex.js
@@ -5,4 +5,13 @@ rapidash: {
 inherit: true,
 types: ["Fire", "Fairy"],
 },
+shiinotic: {
+inherit: true,
+abilities: {0: "Illuminate", 1: "Effect Spore", H: "Triage"},
+},
+gastly: {
+inherit: true,
+types: "Ghost", "Poison", "luke"},
+abilities: {0: "Cursed Body"},
+},
 };
