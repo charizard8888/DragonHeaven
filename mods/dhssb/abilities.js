d7228f2deecc1c6ce9c5215fa108a62f2a130960
UncleFlacco <UncleFlacco382@gmail.com>
2018-02-18 15:35:01 +0545

    Added Juice Drink (#739)
    Again, hope this works!

--- mods/dhssb/abilities.js
+++ mods/dhssb/abilities.js
@@ -676,5 +676,35 @@ exports.BattleAbilities = {
 		id:'girlpower',
 		name:'Girl Power', 
 	},
-
+   'juicedrink': {
+		shortDesc: "Immunity + Unaware",
+		onAnyModifyBoost: function (boosts, target) {
+            let source = this.effectData.target;
+            if (source === target) return;
+            if (source === this.activePokemon && target === this.activeTarget) {
+                boosts['def'] = 0;
+                boosts['spd'] = 0;
+                boosts['evasion'] = 0;
+            }
+            if (target === this.activePokemon && source === this.activeTarget) {
+                boosts['atk'] = 0;
+                boosts['spa'] = 0;
+                boosts['accuracy'] = 0;
+            }
+        },
+      onUpdate: function (pokemon) {
+            if (pokemon.status === 'psn' || pokemon.status === 'tox') {
+                this.add('-activate', pokemon, 'ability: Juice Drink');
+                pokemon.cureStatus();
+            }
+        },
+      onSetStatus: function (status, target, source, effect) {
+          if (status.id !== 'psn' && status.id !== 'tox') return;
+          if (!effect || !effect.status) return false;
+          this.add('-immune', target, '[msg]', '[from] ability: Juice Drink');
+          return false;
+      },
+		id: "juicedrink",
+		name: "Juice Drink",
+	},
 };
