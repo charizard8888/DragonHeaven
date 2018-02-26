790518bdecff6ca02526c96798fc18934c8fa1b8
charizard8888 <omkumr082001@outlook.com>
2018-02-26 16:53:16 +0530

    Update abilities.js

--- mods/sylvemons/abilities.js
+++ mods/sylvemons/abilities.js
@@ -2,7 +2,7 @@
 
 exports.BattleAbilities = {
 
-/* Magic Healing	This Pokemon heals 6% of their HP every turn.
+/* 
 Ethereal Fist	Punch Moves become Special and gain 1.2x damage (Same Moves effected as Iron Fist).
 Disperal	Boosts Bullet Seed, Seed Bomb, and Seed Flare by 1.2x power, and Leech Seed deals 20% more damage and heals 30% more HP each turn.
 Housekeeping	Removes hazards upon switch-in.
@@ -54,4 +54,122 @@ Swarm	Boosts the power of Bug-type moves by 1.5x.
 		rating: 3,
 		num: 89,
 	},*/
+	"swarm": {
+		desc: "When this Pokemon has 1/2 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Bug-type attack.",
+		shortDesc: "This Pokemon's attacking stat is 1.5x with Bug attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Bug') {
+				this.debug('Swarm boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Bug') {
+				this.debug('Swarm boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "swarm",
+		name: "Swarm",
+		rating: 2,
+		num: 68,
+	},
+	"blaze": {
+		desc: "When this Pokemon has 1/2 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Fire-type attack.",
+		shortDesc: "This Pokemon's attacking stat is 1.5x with Fire attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Fire') {
+				this.debug('Blaze boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Fire') {
+				this.debug('Blaze boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "blaze",
+		name: "Blaze",
+		rating: 2,
+		num: 66,
+	},
+	"torrent": {
+		desc: "When this Pokemon has 1/2 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Water-type attack.",
+		shortDesc: "This Pokemon's attacking stat is 1.5x with Water attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('Torrent boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('Torrent boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "torrent",
+		name: "Torrent",
+		rating: 2,
+		num: 67,
+	},
+	"overgrow": {
+		desc: "When this Pokemon has 1/2 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Grass-type attack.",
+		shortDesc: "This Pokemon's attacking stat is 1.5x with Grass attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Grass') {
+				this.debug('Overgrow boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Grass') {
+				this.debug('Overgrow boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "overgrow",
+		name: "Overgrow",
+		rating: 2,
+		num: 65,
+	},
+	"technician": {
+		desc: "This Pokemon's moves of 60 power or less have their power multiplied by 1.5. Does affect Struggle.",
+		shortDesc: "This Pokemon's moves of 60 power or less have 1.5x power. Includes Struggle.",
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, defender, move) {
+			if (basePower <= 75) {
+				this.debug('Technician boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "technician",
+		name: "Technician",
+		rating: 4,
+		num: 101,
+	},
+	"megalauncher": {
+		desc: "This Pokemon's pulse, ball and bomb moves have their power multiplied by 1.5. Heal Pulse restores 3/4 of a target's maximum HP, rounded half down.",
+		shortDesc: "This Pokemon's pulse, ball and bomb moves have 1.5x power. Heal Pulse heals 3/4 target's max HP.",
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, defender, move) {
+			if (move.flags['pulse' || 'bullet']) {
+				return this.chainModify(1.5);
+			}
+		},
+		id: "megalauncher",
+		name: "Mega Launcher",
+		rating: 3.5,
+		num: 178,
+	},
+	
 };
