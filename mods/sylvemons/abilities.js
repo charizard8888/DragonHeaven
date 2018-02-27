7c1975a2559fb8b9fac15fc79b25108193cb85af
charizard8888 <omkumr082001@outlook.com>
2018-02-27 15:14:30 +0530

    Update abilities.js

--- mods/sylvemons/abilities.js
+++ mods/sylvemons/abilities.js
@@ -17,10 +17,6 @@ Guard Up	Upon switch-in, this Pokemon's Defense or Special Defense goes up by 1 
 Scrappy	Ignores every type inmunity when attacking.
 Technician	This Pokemon's moves of 75 power or less have 1.5x power. Includes Struggle. 
 Obstinacy	User gains a boost in it's moves the lower it's HP gets. Formula:  (1.0 - [Current percentage of HP in decimal form]) + 1.0
-Overgrow	Boosts the power of Grass-type moves by 1.5x.
-Blaze	Boosts the power of Fire-type moves by 1.5x.
-Torrent	Boosts the power of Water-type moves by 1.5x.
-Swarm	Boosts the power of Bug-type moves by 1.5x.
 */
 
 	"magichealing": {
@@ -171,5 +167,136 @@ Swarm	Boosts the power of Bug-type moves by 1.5x.
 		rating: 3.5,
 		num: 178,
 	},
-	
+	"thickfat": {
+		desc: "If a Pokemon uses a Fire- or Ice-type attack against this Pokemon, that Pokemon's attacking stat is halved when calculating the damage to this Pokemon.",
+		shortDesc: "Fire/Ice-type moves against this Pokemon deal damage with a halved attacking stat. Immune to hail damage, burn, and freeze.",
+		onModifyAtkPriority: 6,
+		onSourceModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Ice' || move.type === 'Fire') {
+				this.debug('Thick Fat weaken');
+				return this.chainModify(0.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onSourceModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Ice' || move.type === 'Fire') {
+				this.debug('Thick Fat weaken');
+				return this.chainModify(0.5);
+			}
+		},
+		onImmunity: function (type, pokemon) {
+			if (type === 'hail') return false;
+		},
+		onUpdate: function (pokemon) {
+			if (pokemon.status === 'brn' || pokemon.status === 'frz') {
+				this.add('-activate', pokemon, 'ability: Thick Fat');
+				pokemon.cureStatus();
+			}
+		},
+		id: "thickfat",
+		name: "Thick Fat",
+		rating: 3.5,
+		num: 47,
+	},
+	"liquidvoice": {
+		desc: "This Pokemon's sound-based moves become Water-type moves. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
+		shortDesc: "This Pokemon's sound-based moves become Water type and power up by 1.2x times.",
+		onModifyMovePriority: -1,
+		onModifyMove: function (move) {
+			if (move.flags['sound']) {
+				move.type = 'Water';
+				move.liquidvoiceBoosted = true;
+			}
+		},
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, pokemon, target, move) {
+			if (move.liquidvoiceBoosted) return this.chainModify([0x1333, 0x1000]);
+		},
+		id: "liquidvoice",
+		name: "Liquid Voice",
+		rating: 2.5,
+		num: 204,
+	},
+	"scrappy": {
+		shortDesc: "This Pokemon can hit Ghost types with Normal- and Fighting-type moves.",
+		onModifyMovePriority: -5,
+		onModifyMove: function (move) {
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity = true;
+			}
+		},
+		id: "scrappy",
+		name: "Scrappy",
+		rating: 3,
+		num: 113,
+	},
+	"guardup": {
+		desc: "On switch-in, this Pokemon's Attack or Special Attack is raised by 1 stage based on the weaker combined defensive stat of all opposing Pokemon. Attack is raised if their Defense is lower, and Special Attack is raised if their Special Defense is the same or lower.",
+		shortDesc: "Upon switch-in, this Pokemon's Defense or Special Defense goes up by 1 stage depending on the opponent's higher attacking stat (Download clone).",
+		onStart: function (pokemon) {
+			let foeactive = pokemon.side.foe.active;
+			let totalatk = 0;
+			let totalspa = 0;
+			for (let i = 0; i < foeactive.length; i++) {
+				if (!foeactive[i] || foeactive[i].fainted) continue;
+				totalatk += foeactive[i].getStat('atk', false, true);
+				totalspa += foeactive[i].getStat('spa', false, true);
+			}
+			if (totalatk && totalspa >= totalspa) {
+				this.boost({spd: 1});
+			} else if (totalspa) {
+				this.boost({def: 1});
+			}
+		},
+		id: "guardup",
+		name: "Guard Up",
+		rating: 4,
+		num: 88,
+	},
+	"corrosion": {
+		inherit: true,
+		onModifyMovePriority: -5,
+		onModifyMove: function (move) {
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity['Steel'] = true;
+				move.ignoreImmunity['Poison'] = true;
+			}
+		},
+		id: "corrosion",
+		name: "Corrosion",
+		rating: 3,
+		num: 113,
+	},
+	"flareboost": {
+		desc: "While this Pokemon is burned, the power of its special attacks is multiplied by 1.5.",
+		shortDesc: "While this Pokemon is burned, its special attacks have 2x power.",
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, defender, move) {
+			if (attacker.status === 'brn' && move.category === 'Special') {
+				return this.chainModify(2);
+			}
+		},
+		id: "flareboost",
+		name: "Flare Boost",
+		rating: 2.5,
+		num: 138,
+	},
+	"housekeeping": {
+		shortDesc: "Removes hazards upon switch-in.",
+		onStart: function (pokemon) {
+				if (pokemon.hp && pokemon.removeVolatile('leechseed')) {
+					this.add('-end', pokemon, 'Leech Seed', '[from] ability: Housekeeping', '[of] ' + pokemon);
+				}
+				let sideConditions = ['spikes', 'toxicspikes', 'stealthrock', 'stickyweb'];
+				for (const condition of sideConditions) {
+					if (pokemon.hp && pokemon.side.removeSideCondition(condition)) {
+						this.add('-sideend', pokemon.side, this.getEffect(condition).name, '[from] ability: Housekeeping', '[of] ' + pokemon);
+					}
+			},
+		},
+		id: "housekeeping",
+		name: "Housekeeping",
+	},
 };
