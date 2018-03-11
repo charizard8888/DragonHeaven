04dad0df6dad40e897112c839e442744ef1f4e8a
charizard8888 <omkumr082001@outlook.com>
2018-03-11 15:10:00 +0530

    Update abilities.js

--- mods/eeveed/abilities.js
+++ mods/eeveed/abilities.js
@@ -90,6 +90,14 @@ exports.BattleAbilities = {
 		name: "Unbreakable",
 		rating: 2,
 	},
+	"lucky": {
+		shortDesc: "This mons attacks always crit unless Lucky Shield, shell armour or battle armour are active.",
+		onModifyCritRatio: function (critRatio) {
+			return critRatio + 6;
+		},
+		id: "lucky",
+		name: "Lucky",
+	},
 		"reaperslice": { /* Make the 1.2 somehow 1.3 + Add the infiltrating effect*/
 		desc: "Ghost type moves can bypass Subsitutes, they have also x1.3 power",
 		shortDesc: "Ghost type moves can bypass Subsitutes, they have also x1.3 power",
