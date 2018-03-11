9cac186553991e9c31d3b74ef8fac10ae809fed4
charizard8888 <omkumr082001@outlook.com>
2018-03-11 15:04:47 +0530

    Update formats-data.js

--- mods/eeveed/formats-data.js
+++ mods/eeveed/formats-data.js
@@ -5,4 +5,42 @@ buzzasoarmega: {
 requiredItem: "Buzzasoarite",
 tier: "OU",
 }, 
+magzormega: {
+requiredItem: "magzorite",
+tier: "OU",
+}, 
+	reapthermega: {
+requiredItem: "Reaptherite",
+tier: "OU",
+	},
+scyanidemega: {
+requiredItem: "Scyanideite",
+tier: "OU",
+}, 
+	scypsymega: {
+requiredItem: "Scypsyite",
+tier: "OU",
+}, 
+	alunixmega: {
+requiredItem: "Alunixite",
+tier: "OU",
+}, 
+	coffilixmega: {
+requiredItem: "Coffilixite",
+tier: "OU",
+}, 
+crystixmega: {
+requiredItem: "Crystixite",
+tier: "OU",
+}, 
+	gemelixmega: {
+requiredItem: "Gemelixite",
+tier: "OU",
+}, 
+	scarixmega: {
+requiredItem: "Scarixite",
+tier: "OU",
+}, 
+
+
 };
