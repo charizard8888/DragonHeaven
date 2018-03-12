c8e113441f554f5bad14cd4f8bdabf3c05a3561f
charizard8888 <omkumr082001@outlook.com>
2018-03-12 17:02:34 +0530

    Create scripts.js

---
+++ mods/typeopt/scripts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.BattleScripts = {
+
+this.modData('Learnsets', 'vanilluxe').learnset.nastyplot = ['6L1'];
+this.modData('Learnsets', 'vanilluxe').learnset.hypervoice = ['6L1'];
+this.modData('Learnsets', 'vanilluxe').learnset.focusblast = ['6L1'];
+
+};
