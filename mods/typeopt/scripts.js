99936c9ae00fa511907557b787e9db5d769e6aaa
charizard8888 <omkumr082001@outlook.com>
2018-03-12 17:04:24 +0530

    Update scripts.js

--- mods/typeopt/scripts.js
+++ mods/typeopt/scripts.js
@@ -1,9 +1,10 @@
 'use strict';
 
 exports.BattleScripts = {
+init: function () {
 
 this.modData('Learnsets', 'vanilluxe').learnset.nastyplot = ['6L1'];
 this.modData('Learnsets', 'vanilluxe').learnset.hypervoice = ['6L1'];
 this.modData('Learnsets', 'vanilluxe').learnset.focusblast = ['6L1'];
-
+	},
 };
