7637abece878a765889b46ec69ec1d7868169037
charizard8888 <omkumr082001@outlook.com>
2018-03-16 16:06:35 +0530

    Update pokedex.js

--- mods/lukemod/pokedex.js
+++ mods/lukemod/pokedex.js
@@ -1,73 +1,114 @@
 'use strict'
 exports.BattlePokedex = {
-rapidash: {
-inherit: true,
-types: ["Fire", "Fairy"],
-},
-shiinotic: {
-inherit: true,
-abilities: {0: "Illuminate", 1: "Effect Spore", H: "Triage"},
-},
-gastly: {
-inherit: true,
-types: ["Ghost", "Poison", "Luke"],
-abilities: {0: "Cursed Body"},
-},
-haunter: {
-inherit: true,
-types: ["Ghost", "Poison", "Luke"],
-abilities: {0: "Cursed Body"},
-},
-koffing: {
-inherit: true,
-types: ["Poison", "Fire", "Luke"],
-abilities: {0: "Flash Fire", H: "Aftermath"},
-},
-weezing: {
-inherit: true,
-types: ["Poison", "Fire", "Luke"],
-abilities: {0: "Flash Fire", H: "Aftermath"},
-},
-	
-// Mega changes
-charizardmegax: {
-inerit: true,
-baseStats: {hp: 78, atk: 150, def: 111, spa: 100, spd: 85, spe: 110},
-},
-charizardmegay: {
-inerit: true,
-baseStats: {hp: 78, atk: 84, def: 78, spa: 159, spd: 115, spe: 120},
-},
-garchompmega: {
-inerit: true,
-abilities: {0: "Defiant"},
-},
-gallademega: {
-inerit: true,
-abilities: {0: "Hyper Cutter"},
-},
-ampharosmega: {
-inerit: true,
-abilities: {0: "Regenerator"},
-},
-houndoommega: {
-inerit: true,
-abilities: {0: "Weak Armor"},
-},
-absolmega: {
-inerit: true,
-types: ["Dark", "Fairy"],
-},
-sceptilemega: {
-inerit: true,
-abilities: {0: "Adaptability"},
-},
-steelixmega: {
-inerit: true,
-abilities: {0: "Iron Barbs"},
-},
-alakazammega: {
-inerit: true,
-abilities: {0: "Dazzling"},
-},
+	rapidash: {
+		inherit: true,
+		types: ["Fire", "Fairy"],
+	},
+	shiinotic: {
+		inherit: true,
+		abilities: {
+			0: "Illuminate",
+			1: "Effect Spore",
+			H: "Triage"
+		},
+	},
+	gastly: {
+		inherit: true,
+		types: ["Ghost", "Poison", "Luke"],
+		abilities: {
+			0: "Cursed Body"
+		},
+	},
+	haunter: {
+		inherit: true,
+		types: ["Ghost", "Poison", "Luke"],
+		abilities: {
+			0: "Cursed Body"
+		},
+	},
+	koffing: {
+		inherit: true,
+		types: ["Poison", "Fire", "Luke"],
+		abilities: {
+			0: "Flash Fire",
+			H: "Aftermath"
+		},
+	},
+	weezing: {
+		inherit: true,
+		types: ["Poison", "Fire", "Luke"],
+		abilities: {
+			0: "Flash Fire",
+			H: "Aftermath"
+		},
+	},
+	// Mega changes
+	charizardmegax: {
+		inerit: true,
+		baseStats: {
+			hp: 78,
+			atk: 150,
+			def: 111,
+			spa: 100,
+			spd: 85,
+			spe: 110
+		},
+	},
+	charizardmegay: {
+		inerit: true,
+		baseStats: {
+			hp: 78,
+			atk: 84,
+			def: 78,
+			spa: 159,
+			spd: 115,
+			spe: 120
+		},
+	},
+	garchompmega: {
+		inerit: true,
+		abilities: {
+			0: "Defiant"
+		},
+	},
+	gallademega: {
+		inerit: true,
+		abilities: {
+			0: "Hyper Cutter"
+		},
+	},
+	ampharosmega: {
+		inerit: true,
+		abilities: {
+			0: "Regenerator"
+		},
+	},
+	houndoommega: {
+		inerit: true,
+		abilities: {
+			0: "Weak Armor"
+		},
+	},
+	absolmega: {
+		inerit: true,
+		types: ["Dark", "Fairy"],
+	},
+	sceptilemega: {
+		inerit: true,
+		abilities: {
+			0: "Adaptability"
+		},
+	},
+	steelixmega: {
+		inerit: true,
+		abilities: {
+			0: "Iron Barbs"
+		},
+	},
+	alakazammega: {
+		inerit: true,
+		abilities: {
+			0: "Dazzling"
+		},
+	},
 };
