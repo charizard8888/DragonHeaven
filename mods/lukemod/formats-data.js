4f98517e49e09ad7215e3a54de320c94f792ab59
charizard8888 <omkumr082001@outlook.com>
2018-03-16 17:05:27 +0530

    Create formats-data.js

---
+++ mods/lukemod/formats-data.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports.BattleFormatsData = {
+
+zangoosemega: {
+randomBattleMoves: ["", "", "", "", "",], 
+randomDoubleBattleMoves: ["", "", "", "", "",],
+requiredItem: "Zangooseite",
+tier: "OU",
+}, 
+sevipermega: {
+randomBattleMoves: ["", "", "", "", "",], 
+randomDoubleBattleMoves: ["", "", "", "", "",],
+requiredItem: "Sevipite",
+tier: "OU",
+}, 
+flygommega: {
+randomBattleMoves: ["", "", "", "", "",], 
+randomDoubleBattleMoves: ["", "", "", "", "",],
+requiredItem: "Flygomite",
+tier: "OU",
+}, 
+hydreigonmega: {
+randomBattleMoves: ["", "", "", "", "",], 
+randomDoubleBattleMoves: ["", "", "", "", "",],
+requiredItem: "Hydreigonite",
+tier: "OU",
+}, 
+};
