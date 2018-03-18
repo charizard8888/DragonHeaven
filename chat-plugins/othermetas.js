195899135dff534d979bf63aa120e114780308e1
charizard8888 <omkumr082001@outlook.com>
2018-03-18 15:40:50 +0530

    Update othermetas.js

--- chat-plugins/othermetas.js
+++ chat-plugins/othermetas.js
@@ -762,10 +762,12 @@ gutter: function (target, room, user) {
 		let feDex = require('../mods/fe/pokedex.js').BattlePokedex;
 		if (!feDex) return this.errorReply("Error Fetching FE Data.");
 		Object.values(feDex).forEach(mon => {
+			let speedtierplusscarf = (2.2 * mon.baseStats.spe + 108.9) * 1.5;
+			let speedtierscarf = (2 * mon.baseStats.spe + 99) * 1.5;
 			let speedtierplus = 2.2 * mon.baseStats.spe + 108.9;
 			let speedtier = 2 * mon.baseStats.spe + 99;
 			let speedtierzero = 2 * mon.baseStats.spe + 36;
-			buf += `${speedtierplus}: Fast+ ${mon.species}<br>${speedtier}: Fast ${mon.species}<br>${speedtierzero}: Bulky ${mon.species}<br>`;
+			buf += `${speedtierplusscarf}: Scarf Fast+ ${mon.species}<br>${speedtierscarf}: Scarf Fast ${mon.species}<br>${speedtierplus}: Fast+ ${mon.species}<br>${speedtier}: Fast ${mon.species}<br>${speedtierzero}: Bulky ${mon.species}<br>`;
 		});
 		this.sendReplyBox(`${buf}</div>`);
 	},
@@ -775,10 +777,12 @@ gutter: function (target, room, user) {
 		let feDex = require('../mods/eternal/pokedex.js').BattlePokedex;
 		if (!feDex) return this.errorReply("Error Fetching FE Data.");
 		Object.values(feDex).forEach(mon => {
+			let speedtierplusscarf = (2.2 * mon.baseStats.spe + 108.9) * 1.5;
+			let speedtierscarf = (2 * mon.baseStats.spe + 99) * 1.5;
 			let speedtierplus = 2.2 * mon.baseStats.spe + 108.9;
 			let speedtier = 2 * mon.baseStats.spe + 99;
 			let speedtierzero = 2 * mon.baseStats.spe + 36;
-			buf += `${speedtierplus}: Fast+ ${mon.species}<br>${speedtier}: Fast ${mon.species}<br>${speedtierzero}: Bulky ${mon.species}<br>`;
+			buf += `${speedtierplusscarf}: Scarf Fast+ ${mon.species}<br>${speedtierscarf}: Scarf Fast ${mon.species}<br>${speedtierplus}: Fast+ ${mon.species}<br>${speedtier}: Fast ${mon.species}<br>${speedtierzero}: Bulky ${mon.species}<br>`;
 		});
 		this.sendReplyBox(`${buf}</div>`);
 	},
